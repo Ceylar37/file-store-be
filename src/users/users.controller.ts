05048ad5d753ae0437f73cf1a8dd756ab2964a9e
ceylar37 <ceylar37@gmail.com>
2022-11-15 17:27:18 +0300

    fix deploy

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { UsersService } from './users.service';
-import { ReadableUserDto } from './dto/readableUser.dto';
-import { ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from '../auth/auth.guard';
+import { Controller, Get } from '@nestjs/common';
 
-@ApiTags('Users')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor() {}
 
-  @ApiOperation({ summary: 'All users' })
-  @ApiResponse({
-    status: 200,
-    type: [ReadableUserDto],
-  })
-  @UseGuards(AuthGuard)
   @Get()
-  getAll() {
-    return this.usersService.findAll();
+  getHi() {
+    return 'Hello world';
   }
 }
