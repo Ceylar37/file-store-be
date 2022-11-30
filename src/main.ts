000abdd1acfeea1f113013e52ba5bd0262d61e47
ceylar37 <ceylar37@gmail.com>
2022-11-30 18:40:14 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+console.log(process.env.CLIENT_URL);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { credentials: true, origin: process.env.CLIENT_URL },
+    cors: {
+      credentials: true,
+      origin: process.env.CLIENT_URL,
+      methods: '*',
+    },
   });
   app.use(cookieParser());
   app.enableCors();
