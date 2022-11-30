d39833954e73f176fdf0da10bcc43ea1a2cf9967
ceylar37 <ceylar37@gmail.com>
2022-11-30 15:29:53 +0300

    remove origins from env

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
-const whitelist = process.env.ORIGINS.split(',');
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
     credentials: true,
-    origin: (origin, callback) => {
-      console.log(origin, whitelist);
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: 'http://localhost:3000',
     methods: '*',
   });
   const config = new DocumentBuilder().setTitle('File store').build();
