a4bdb36234798db286425f290ee9ec5a6cbeb76c
ceylar37 <ceylar37@gmail.com>
2022-11-30 14:51:51 +0300

    add origin to env

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,20 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+const whitelist = process.env.ORIGINS;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: (origin, callback) => {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: '*',
   });
   const config = new DocumentBuilder().setTitle('File store').build();
